6a81b04a8f0a7006859301c24141fbc0dc6469ad
hmode3213 <47697661+hmode3213@users.noreply.github.com>
2019-02-18 03:02:59 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+} else if(msg.content.startsWith(`${PREFIX}skip`)) {
+        if(!msg.member.voiceChannel){
+           var embedskip1 = new Discord.RichEmbed()
+                .setTitle(`You are in not in the Voice Channel!`)
+                .setColor(['#f9fcfc'])
+            return msg.channel.sendEmbed(embedskip1); 
+        }
+        if(!serverQueue){
+            var embedskip2 = new Discord.RichEmbed()
+                .setTitle(`There is nothing to Skip!`)
+                .setColor(['#f9fcfc'])
+            return msg.channel.sendEmbed(embedskip2);
+        }
+        serverQueue.connection.dispatcher.end('Skip command has been used!');
+        var embedskip3 = new Discord.RichEmbed()
+            .setTitle(`⏩Skipped👍`)
+            .setColor(['#f9fcfc'])
+        return msg.channel.sendEmbed(embedskip3);
+    }
